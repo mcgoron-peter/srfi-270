093aa4d84808436c8beb523f7140aaac910d1c06
Peter McGoron <code@mcgoron.com>
2026-05-09 20:30:03 -0400

    sample implementation of hex floats in Chez 10.4.0

---
+++ test.scm
@@ -0,0 +1,87 @@
+;;;; SPDX-FileCopyrightText: 2026 Peter McGoron
+;;;; SPDX-License-Identifier: MIT
+
+(import (rnrs) (chezscheme))
+(define (r s) (read (open-string-input-port s)))
+(define (test str n)
+  (let ((number (r str)))
+    (unless (eqv? n (r str))
+      (display `(,str (,number) != ,n))
+      (newline))))
+
+(test "#e#x1.2p3" 9)
+(test "#e#x9p9" 4608)
+(test "#e#xFE.FFp1" 65279/128)
+(test "#e#x-0.Ap-2" -5/32)
+(test "#e#x1.9p1+10p1i" 25/8+32i)
+
+(define prng (make-pseudo-random-generator))
+(define (roll n)
+  (pseudo-random-generator-next! prng n))
+(define (random-digit)
+  (let ((val (roll 16)))
+    (values val
+            (case val
+              ((0) #\0)
+              ((#x1) #\1)
+              ((#x2) #\2)
+              ((#x3) #\3)
+              ((#x4) #\4)
+              ((#x5) #\5)
+              ((#x6) #\6)
+              ((#x7) #\7)
+              ((#x8) #\8)
+              ((#x9) #\9)
+              ((#xA) #\A)
+              ((#xB) #\B)
+              ((#xC) #\C)
+              ((#xD) #\D)
+              ((#xE) #\E)
+              ((#xF) #\F)))))
+
+(define (generate-pair)
+  (let* ((mantissa-hex-length (roll 52/4))
+         (exponent (- (roll 2048) 1023))
+         (sign (if (zero? (roll 2))
+                   #\+
+                   #\-))
+         (fac (* (case sign
+                   ((#\+) 1)
+                   ((#\-) -1))
+                 (expt 2 exponent))))
+    (cond
+     ((= exponent 1024)    ; no NaN, inf
+      (generate-pair))
+     (else
+      (let loop ((str '())
+                 (n (if (= exponent -1023)
+                        0
+                        1))
+                 (i mantissa-hex-length))
+        (cond
+          ((and (zero? i) (not (= exponent -1023)))
+           (values (string-append "#e#x"
+                                  (string sign)
+                                  "1."
+                                  (list->string str)
+                                  "p"
+                                  (number->string exponent))
+                   (* n fac)))
+          ((zero? i)    ; zero and subnormal case
+           (values (string-append "#e#x"
+                                  (string sign)
+                                  "0."
+                                  (list->string str)
+                                  "p"
+                                  (number->string exponent))
+                    (* n fac)))
+          (else
+           (let-values (((val dig) (random-digit)))
+             (loop (cons dig str)
+                   (+ n (* val (expt 16 (* i -1))))
+                   (- i 1))))))))))
+
+(do ((i 0 (+ i 1)))
+    ((= i 10000))
+  (call-with-values generate-pair test))
+
